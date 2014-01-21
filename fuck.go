3d087ac1a3597590bd041a54a7c642786d55b8ca
Chris Barry <chris@barry.im>
2014-01-20 20:06:28 -0500

    Cleaning up input

--- fuck.go
+++ fuck.go
@@ -3,21 +3,20 @@ package main
 import(
 	"os"
 	"fmt"
-	"io/ioutil"
 	"bufio"
+	"io/ioutil"
 )
 
-var state [3000]byte
+var state [65535]byte
 var index = 0
 
 var whileIndex = 0
-var whileState = 0
 
 func main() {
 	p, err := ioutil.ReadFile(os.Args[1])
 	if err != nil {
 		fmt.Println("Did you supply a file?")
-		panic(err)
+		return
 	}
 	reader := bufio.NewReader(os.Stdin)
 	for i := 0; i < len(p); i++ {
@@ -29,14 +28,13 @@ func main() {
 		case "-": state[index]--
 
 		case ".": fmt.Print(string(state[index]))
-		case ",":
-			input, _ := reader.ReadString('\n')
-			state[index] = []byte(input)[0]
+		case ",": state[index], _ = reader.ReadByte()
 
 		// TODO: I don't think this works.
 		case "[":
 			whileIndex, whileState = index, i
 			fmt.Println("HEHE")
+
 		case "]":
 			fmt.Println(state[whileIndex])
 			if state[whileIndex] != 0 {
